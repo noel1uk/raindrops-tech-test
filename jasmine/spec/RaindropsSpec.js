de337e021a715ea5936b2057b3caf19be1a2aa90
Noel Vock <noel1uk@gmail.com>
2018-05-16 15:41:40 +0100

    test: ensure it returns number its given when number does not have 3 as a factor

--- jasmine/spec/RaindropsSpec.js
+++ jasmine/spec/RaindropsSpec.js
@@ -1,6 +1,16 @@
 describe('raindrops', function() {
 
-  it('returns "Pling" when given a number which has 3 as a factor', function() {
-    expect(raindrops(3)).toEqual('Pling');
+  describe('when given a number that has 3 as a factor', function() {
+
+    it('returns "Pling"', function() {
+      expect(raindrops(3)).toEqual('Pling');
+    });
+  })
+
+  describe("when given a number that doesn't have 3 as a factor", function() {
+
+    it('returns the number', function() {
+      expect(raindrops(1)).toEqual(1);
+    });
   });
 });
