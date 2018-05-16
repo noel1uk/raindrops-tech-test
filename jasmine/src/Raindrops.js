0a60684607c95cfb457398a004c1321f0dee64da
Noel Vock <noel1uk@gmail.com>
2018-05-16 15:50:04 +0100

    refactor: to keep code DRY

--- jasmine/src/Raindrops.js
+++ jasmine/src/Raindrops.js
@@ -1,9 +1,6 @@
 function raindrops(number) {
-  if (number === 1 ) {
-    return 1;
-  }  
-  if ( number === 2 ){
-    return 2;
+  if ( number < 3 ) {
+    return number;
   }
   return 'Pling';
 }
